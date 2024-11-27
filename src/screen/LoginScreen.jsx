6a635182085812a2f926c52590d0cbe6e4ed02c5
11sdz <danielzinn641@gmail.com>
2024-11-27 15:53:46 +0200

    login Screen text inputs

--- src/screen/LoginScreen.jsx
+++ src/screen/LoginScreen.jsx
@@ -1,14 +1,26 @@
-import { StyleSheet, Text, View ,Image} from 'react-native'
+import { StyleSheet, Text, View ,Image, TextInput} from 'react-native'
 import React from 'react'
+import { FontAwesome } from '@expo/vector-icons'
 
 const LoginScreen = () => {
   return (
     <View style={styles.container}>
         <View style={styles.loginBackgrondContainer}>
-            <Image source={require("../assets/loginBackground.png")} style={styles.backgroundImage}/>
+            <Image source={require("../assets/topHouse.png")} style={styles.backgroundImage}/>
         </View>
       <View style={styles.helloContainer}>
-        <Text style={styles.helloText}>FamilyNest</Text>
+        <Text style={styles.helloText}>Hello</Text>
+      </View>
+      <View>
+        <Text style={styles.loginText}>Login to your account</Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <FontAwesome name={"user"} size={28} color={"#9A9A9A"} style={styles.inputIcon}/>
+        <TextInput style={styles.textInput} placeholder='Email adress'/>
+      </View>
+      <View style={styles.inputContainer}>
+        <FontAwesome name={"lock"} size={28} color={"#9A9A9A"} style={styles.inputIcon}/>
+        <TextInput style={styles.textInput} placeholder='password'/>
       </View>
     </View>
   )
@@ -18,19 +30,40 @@ export default LoginScreen
 
 const styles = StyleSheet.create({
     container:{
-        backgroundColor: "F5F5F5",
+        backgroundColor: "#E4F1F4",
         flex: 1
     },loginBackgrondContainer:{
       height:300,
     },backgroundImage:{
+      marginTop:10,
       height:"100%",
       width:"100%"
     },helloContainer:{
     },helloText:{
       textAlign:"center",
-      fontSize: 70,
+      fontSize: 60 ,
       fontWeight: 700,
-      color: "#262626"
+      color: "#542F2F",
+    },loginText:{
+      textAlign:"center",
+      fontSize: 25,
+      fontWeight:500,
+      color: "#542F2F",
+      marginBottom:10
+    },inputContainer:{
+      backgroundColor:"#FFFFFF",
+      flexDirection:"row",
+      borderRadius:20,
+      marginHorizontal:40,
+      elevation:10,
+      marginVertical:10,
+    },inputIcon:{
+      marginLeft:15,
+      alignSelf:"baseline",
+      marginTop:15
+    },textInput:{
+      fontSize:18,
+      flex:1,
+      marginLeft:10,
     }
-
 })
