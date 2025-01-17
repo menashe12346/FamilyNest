e3b1101a9283135ffc44769ca56822d43a7acb01
11sdz <danielzinn641@gmail.com>
2025-01-17 13:31:39 +0200

    todo rewards

--- src/screens/RewardsScreen.jsx
+++ src/screens/RewardsScreen.jsx
@@ -3,6 +3,32 @@ import { useDispatch, useSelector } from "react-redux";
 import React from "react";
 import { getProfileById } from "../utils/ProfileUtils";
 
+
+/**
+ * TODO
+ * 
+ * 1.let parents set and active rewards and time period (can use our component DateTimePicker)
+ * 2.show some leadboard chart for active rewards competition for example https://docs.appspace.com/wp-content/uploads/2020/10/Leaderboard-1024x575.png
+ * 3.option to look on previous competitions
+ * 
+ * competition:{
+ * [
+ * startTime: date
+ * endTime: date
+ * rewards:[...]  (optional rank 1 rank 2 rank 3)
+ * groupReward  (optional)
+ * participants:[{profileID, score}...]
+ * 
+ * ***** basic things ******
+ * ***** open to changes *****
+ * ]}
+ * 
+ *  * may be only 1
+ * rewards:[{rank1,prize},....{rank2,prize}....]
+ * 
+ * 
+ */
+
 const RewardsScreen = () => {
   const user = useSelector((state) => state.user.user);
   const selectedUser = useSelector(
